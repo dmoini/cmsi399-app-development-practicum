d3f7599f489ebe494f6c9f05b5bb1ed2f6e59a37
Donovan Moini <donovanmoini@gmail.com>
2020-04-02 19:02:39 -0700

    Removed console.logs

--- algorithms/sortCharactersByFrequency.js
+++ algorithms/sortCharactersByFrequency.js
@@ -10,9 +10,7 @@ const frequencySort = s => {
     for (let char of s) {
         count[char] ? count[char]++ : count[char] = 1
     }
-    console.log(count)
     const sorted_count = Object.keys(count).sort((a,b) => count[b] - count[a])
-    console.log(sorted_count)
     let res = ""
     sorted_count.forEach(char => {
         res += char.repeat(count[char])
